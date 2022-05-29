1f5bb6f4effebeeebdc418ef1c09240d41a1c42b
Andrew <andrew.1.kulikov@gmail.com>
2022-05-29 21:30:15 +0300

    HW-19 Add terraform configuration for cluster deployment

---
+++ kubernetes/terraform_cluster/variables.tf
@@ -0,0 +1,33 @@
+variable instance_count {
+  description = "Number of instances"
+  default     = 1
+}
+
+variable cloud_id {
+  description = "Cloud"
+}
+
+variable folder_id {
+  description = "Folder"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+
+variable service_account_key_file {
+  description = "key .json"
+}
+
+variable service_account_id {
+  description = "Service account id"
+}
+
+variable subnet_id {
+  description = "Subnet"
+}
+
+variable network_id {
+  description = "Network"
+}
